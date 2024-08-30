0a40074ebea4b8c377816a304b9365afd06fea25
leonardo-sarda <leonardo_sarda@hotmail.com>
2024-08-30 16:00:26 -0300

    fist

---
+++ exemplo.sql
@@ -0,0 +1,15 @@
+id  | id_veiculo | Catagra | km | diesel | oleo | aditivo | motorista | data_criada | usuario |
+
+
+
+
+usuarios:
+
+id | nome | login | senha | data_criada | matricula | nivel 
+
+
+
+Motorista;
+
+id | nome   | matricula | data_criada |
+
